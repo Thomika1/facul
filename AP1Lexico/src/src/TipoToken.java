3083710ac130433b61184537c5e4b21c81a6bc30
Thomaz Maldonado Bonfim <127054536+Thomika1@users.noreply.github.com>
2025-05-27 14:25:55 -0300

    Delete AP1Lexico directory

--- AP1Lexico/src/src/TipoToken.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public enum TipoToken {
-	PCDec, PCProg, PCInt, PCReal, PCLer, PCImprimir, PCSe, PCEntao, 
-	PCSenao, PCEnqto, PCIni, PCFim, OpAritMult, OpAritDiv, OpAritSoma, OpAritSub,
-	OpRelMenor, OpRelMenorIgual, OpRelMaior, OpRelMaiorIgual, OpRelIgual, OpRelDif,
-	OpBoolE, OpBoolOu, Delim, Atrib, AbrePar, FechaPar,Var, NumInt, NumReal, Cadeia, ERROR, EOF
-}
-
