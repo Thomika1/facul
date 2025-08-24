4dcc06b42f75e0f382d2a7c3356028131d59cf9b
Thomika1 <thomazmbonfim@gmail.com>
2025-08-24 16:36:09 -0300

    add busca.py

--- Sistemas_Inteligentes/Graph/busca.py
+++ Sistemas_Inteligentes/Graph/busca.py
@@ -1,1 +1,21 @@
 
+
+class Estados:
+    def __init__(self, estados):
+        self.estados = estados
+
+class EstadoAponta:
+    def __init__(self, nome, peso):
+        self.nome = nome
+        self.peso = peso
+
+
+
+def main():
+    print(main)
+
+
+
+
+if __name__ =="__main__":
+    main()
