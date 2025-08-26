223c2e30f9f2c3dbbfb9d74d694a8cda1ee4dfd0
Thomika1 <thomazmbonfim@gmail.com>
2025-08-25 22:20:20 -0300

    feat: added initial logic

--- Sistemas_Inteligentes/Graph/busca.py
+++ Sistemas_Inteligentes/Graph/busca.py
@@ -1,5 +1,3 @@
-
-
 class Estados:
     def __init__(self, estadosAponta, nome):
         self.nome = nome
@@ -8,13 +6,33 @@ class Estados:
         return f"Estado(nome='{self.nome}', aponta_para={self.estadosAponta})"
 
 
+def bestPath(graph, startState, finalState) -> list:
+    IT = 0
+    EC = startState
+    LE = [startState]
+    LNE = [startState]
+    BSS = []
+
+    while LNE:
+        if EC == finalState:
+            return LE
+        
+
+
+
+
 
 def main():
     graph = []
-    graph = [Estados(nome="A", estadosAponta={"B":100, "C":125,"D":100,"E":75})]
-
-    estado_a = graph[0]
-    print(estado_a)
+    graph = [Estados(nome="A", estadosAponta={"B":100, "C":125,"D":100,"E":75}),
+             Estados(nome="B", estadosAponta={"A":100, "C":50, "D":75, "E":125}),
+             Estados(nome="C", estadosAponta={"A":125, "B":50, "D":100, "E":125}),
+             Estados(nome="D", estadosAponta={"A":100, "B":75, "C":100, "E":50}),
+             Estados(nome="E", estadosAponta={"A":75, "B":125, "C":125, "D":50})]
+    
+    #print(graph[0].estadosAponta["B"])
+    bestPath(graph=graph,startState=graph[0], finalState=graph[4])
+    
 
 
 if __name__ =="__main__":
